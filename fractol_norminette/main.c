e4e2b5d61dac6a5380650e951dbe52455ed9fbb5
Yasmine Fontenele <yasminefontenele@hotmail.com>
2024-05-11 15:12:36 -0300

    fractol com norminette

---
+++ fractol_norminette/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfontene <yfontene@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 15:17:24 by yfontene          #+#    #+#             */
+/*   Updated: 2024/05/11 10:33:54 by yfontene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	main(int ac, char **av)
+{
+	t_fractal	fractal;
+
+	if (2 == ac && !ft_strncmp(av[1], "mandelbrot", 10)
+		|| 4 == ac && !ft_strncmp(av[1], "julia", 5))
+	{
+		fractal.name = av[1];
+		if (!ft_strncmp(fractal.name, "julia", 5))
+		{
+			fractal.julia_x = atoi_float_to_double(av[2]);
+			fractal.julia_y = atoi_float_to_double(av[3]);
+		}
+		fractal_init(&fractal);
+		fractal_render(&fractal);
+		mlx_loop(fractal.mlx_connection);
+	}
+	else
+	{
+		ft_putstr_fd(ERROR_MESSAGE, STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ fractol_norminette/movements.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movements.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfontene <yfontene@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 20:08:18 by yfontene          #+#    #+#             */
+/*   Updated: 2024/05/11 13:18:37 by yfontene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+/*
+*padrão da biblioteca:
+* int (*f)(void *param)
+*serve para fechar a janela
+*/
+int	close_handler(t_fractal *fractal)
+{
+	mlx_destroy_image(fractal->mlx_connection,
+		fractal->img.img_ptr);
+	mlx_destroy_window(fractal->mlx_connection,
+		fractal->mlx_window);
+	mlx_destroy_display(fractal->mlx_connection);
+	free(fractal->mlx_connection);
+	exit(EXIT_SUCCESS);
+}
+
+/*
+* int (*f)(int keycode, void *param)
+*/
+int	key_handler(int keysym, t_fractal *fractal)
+{
+	if (keysym == XK_Escape)
+		close_handler (fractal);
+	if (keysym == XK_Left)
+		fractal->shift_x += (0.5 * fractal->zoom);
+	else if (keysym == XK_Right)
+		fractal->shift_x -= (0.5 * fractal->zoom);
+	else if (keysym == XK_Up)
+		fractal->shift_y -= (0.5 * fractal->zoom);
+	else if (keysym == XK_Down)
+		fractal->shift_y += (0.5 * fractal->zoom);
+	else if (keysym == XK_plus)
+		fractal->iterations_defintion += 10;
+	else if (keysym == XK_minus)
+		fractal->iterations_defintion -= 10;
+	fractal_render (fractal);
+	return (0);
+}
+
+/*
+* int (*f)(int button, int x, int y, void *param)
+*/
+int	mouse_handler(int button, int x, int y, t_fractal *fractal)
+{
+	if (button == Button5)
+	{
+		fractal->zoom *= 0.95;
+	}
+	else if (button == Button4)
+	{
+		fractal->zoom *= 1.05;
+	}
+	fractal_render(fractal);
+	return (0);
+}
+
+/*
+* Para seguir o mouse e modificar Julia
+* int (*f)(int x, int y, void *param)
+*/
+int	julia_track(int x, int y, t_fractal *fractal)
+{
+	if (!ft_strncmp(fractal->name, "julia", 5))
+	{
+		fractal->julia_x = (map(x, -2, +2, WTH) * fractal->zoom)
+			+ fractal->shift_x;
+		fractal->julia_y = (map(y, +2, -2, HGHT) * fractal->zoom)
+			+ fractal->shift_y;
+		fractal_render(fractal);
+	}
+	return (0);
+}
